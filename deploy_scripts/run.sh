e415d5f83bdf03ac4f14cdfb4c8f07e30b2ddbc7
Claudio Sidi <sidiclau@amazon.com>
2021-08-21 03:21:08 -0700

    start frontend in the background

--- deploy_scripts/run.sh
+++ deploy_scripts/run.sh
@@ -6,4 +6,4 @@ pm2 start index.js
 
 cd /home/ec2-user/frontend
 npm ci
-pm2 start index.js
+pm2 start "npm start"
--- deploy_scripts/validate.sh
+++ deploy_scripts/validate.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 sleep 10
+# validating that the host is up on 8080
 nc -zv 127.0.0.1 8080
