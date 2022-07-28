f0bf3ba2a937c7a71883d8aee7edc27e66498c89
Claudio Sidi <44326322+sidiclaudio@users.noreply.github.com>
2022-07-27 21:11:47 -0700

    Update install.sh
    use NVM as a package manager

--- deploy_scripts/install.sh
+++ deploy_scripts/install.sh
@@ -1,6 +1,13 @@
 # add nodejs to yum
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
-yum install nodejs -y #default-jre ImageMagick
+# curl -sL https://rpm.nodesource.com/setup_lts.x | bash -
+# yum install nodejs -y #default-jre ImageMagick
+
+# Install NVM package manager for manager separate versions of nodejs
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+
+# Install node 14
+nvm install 14
 
 # install pm2 module globaly
 npm install -g pm2
